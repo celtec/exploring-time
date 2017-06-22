fddeed6636c9cd01d88a774c791a4a321c72f7d7
Jorge Alberto dos Santos <jorgealberto.js@gmail.com>
2017-06-22 14:17:00 -0300

    First commit! :seedling:

---
+++ 2017-06-21_rake/testing.rb
@@ -0,0 +1,23 @@
+module Testing
+  def self.run
+    puts "Running..."
+  end
+end
+
+module Testing2
+
+  def self.setup
+    require 'pry';binding.pry
+    puts "Setup..."
+  end
+
+  class << self
+    def run
+      puts "Running again..."
+    end
+  end
+end
+
+Testing.run
+Testing2.run
+Testing2.setup
